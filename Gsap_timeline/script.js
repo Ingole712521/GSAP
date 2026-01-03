def3867d1ecbbcfc43798138e6d3e4908ff15873
nehal7125 <nehal@kshantechsoft.com>
2026-01-03 18:55:44 +0530

    learn about hte timeline

---
+++ Gsap_timeline/script.js
@@ -0,0 +1,26 @@
+/** @format */
+
+var menu = document.querySelector("#nav i");
+var cross = document.querySelector("#full i ");
+var tl = gsap.timeline();
+
+tl.to("#full", {
+  right: 0,
+  duration: 1,
+});
+tl.from("#full h4", {
+  x: 150,
+  duration: 1,
+  stragger: 1,
+  opacity: 0,
+});
+
+tl.pause();
+
+menu.addEventListener("click", function () {
+  tl.play();
+});
+
+cross.addEventListener("click", function () {
+  tl.reverse();
+});
