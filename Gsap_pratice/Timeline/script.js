b4b2ec38533a8c2051dccbea23ccf415065222eb
Ingole712521 <nehal712521@gmail.com>
2026-01-13 23:59:55 +0530

    Update GSAP timeline defaults and comment out example animations

--- Gsap_pratice/Timeline/script.js
+++ Gsap_pratice/Timeline/script.js
@@ -2,10 +2,17 @@
 
 const t1 = gsap.timeline({
   defaults: {
-    delay: 2,
+    delay: 0.5  ,
     duration: 1,
     ease: "power2.out",
+    // repeat: -1,
   },
 });
 
-t1.to(".box1", { x: 100 }).to(".box2", { y: 100 }, "-=1");
+// Overlap
+// t1.to(".box1", { x: 100 }).to(".box2", { y: 100 }, "-=1");
+
+// Start Togeher
+// t1.to(".box1", { x: 100 }).to(".box2", { x: "200" }, "<");
+
+
