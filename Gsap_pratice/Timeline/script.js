aa3b664d88225bfabf34560c426b3f1a115b0e61
Ingole712521 <nehal712521@gmail.com>
2026-01-14 00:24:12 +0530

    Learned Label

--- Gsap_pratice/Timeline/script.js
+++ Gsap_pratice/Timeline/script.js
@@ -2,17 +2,19 @@
 
 const t1 = gsap.timeline({
   defaults: {
-    delay: 0.5  ,
+    delay: 0.5,
     duration: 1,
     ease: "power2.out",
     // repeat: -1,
   },
 });
 
+// Label
+// t1.addLabel("intro");
+// t1.to([".box1", ".box2"], { x: 100 }, "intro");
+
 // Overlap
 // t1.to(".box1", { x: 100 }).to(".box2", { y: 100 }, "-=1");
 
 // Start Togeher
 // t1.to(".box1", { x: 100 }).to(".box2", { x: "200" }, "<");
-
-
